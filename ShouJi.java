74a5c897a315c9565bf4b395d848ac89d5e8c281
mcg <1478427231@qq.com>
2023-03-29 10:12:47 +0800

    第一次提交

---
+++ ShouJi.java
@@ -0,0 +1,11 @@
+//public class ShouJi{
+//	public static void main(String[] args){
+//		String name ="С��9";
+//		int jage =2599;
+//		String jeishao ="����855,6G+128G";
+//		System.out.println(name);
+//		System.out.println(jage);
+//		System.out.println(jeishao);
+//		}
+//
+//}
